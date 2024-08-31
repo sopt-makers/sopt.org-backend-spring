cdc1389fab712f715992e6aae334a9c20624e264
찬기 <limchangi@naver.com>
2024-08-31 18:17:40 +0900

    refac: changing image registry saving point to AWS Public ECR & Readme Update

--- src/main/java/sopt/org/homepage/config/OpenApiConfig.java
+++ src/main/java/sopt/org/homepage/config/OpenApiConfig.java
@@ -1,14 +1,11 @@
 package sopt.org.homepage.config;
 
 
-import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
-
-import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sopt.org.homepage.common.constants.SecurityConstants;
