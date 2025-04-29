d5ca41f94350153558e39615fefd68ade50355b8
찬기 <limchangi@naver.com>
2025-04-29 23:36:27 +0900

    feat: 활동후기 조회 화면 개편사항 작업

--- src/main/java/sopt/org/homepage/common/mapper/ResponseMapper.java
+++ src/main/java/sopt/org/homepage/common/mapper/ResponseMapper.java
@@ -1,7 +1,9 @@
 package sopt.org.homepage.common.mapper;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
-import sopt.org.homepage.review.entity.ReviewEntity;
+
 import sopt.org.homepage.internal.playground.dto.PlaygroundProjectDetailResponse;
 import sopt.org.homepage.internal.playground.dto.PlaygroundProjectResponseDto;
 import sopt.org.homepage.internal.playground.dto.Role;
@@ -12,108 +14,92 @@ import sopt.org.homepage.project.dto.response.ProjectDetailResponseDto;
 import sopt.org.homepage.project.dto.response.ProjectsResponseDto;
 import sopt.org.homepage.project.dto.type.LinkType;
 import sopt.org.homepage.review.dto.response.ReviewsResponseDto;
+import sopt.org.homepage.review.entity.ReviewEntity;
 import sopt.org.homepage.semester.dto.SemesterDao;
 import sopt.org.homepage.semester.dto.SemestersListResponse;
-
-import java.util.List;
 
 @Component
 public class ResponseMapper {
 
-    public SemestersListResponse.SemestersResponse toSemestersResponse(SemesterDao semester) {
-        return new SemestersListResponse.SemestersResponse(
-                semester.id(), semester.color(), semester.logo(), semester.background(), semester.name(), semester.year()
-        );
-    }
+	public SemestersListResponse.SemestersResponse toSemestersResponse(SemesterDao semester) {
+		return new SemestersListResponse.SemestersResponse(
+			semester.id(), semester.color(), semester.logo(), semester.background(), semester.name(), semester.year()
+		);
+	}
 
+	public ProjectsResponseDto toProjectResponse(PlaygroundProjectResponseDto project) {
+		List<Link> links = project.links().stream()
+			.map(link -> new Link(LinkType.fromValue(link.linkTitle()), link.linkUrl()))
+			.toList();
+		Category category = new Category(project.category());
 
+		return ProjectsResponseDto.builder()
+			.id(project.id())
+			.name(project.name())
+			.generation(project.generation())
+			.category(category)
+			.serviceType(project.serviceType())
+			.summary(project.summary())
+			.detail(project.detail())
+			.logoImage(project.logoImage())
+			.thumbnailImage(project.thumbnailImage())
+			.isAvailable(project.isAvailable())
+			.isFounding(project.isFounding())
+			.links(links)
+			.build();
+	}
 
-    public ProjectsResponseDto toProjectResponse(PlaygroundProjectResponseDto project) {
-        List<Link> links = project.links().stream()
-                .map(link -> new Link(LinkType.fromValue(link.linkTitle()), link.linkUrl()))
-                .toList();
-        Category category = new Category(project.category());
+	public ProjectDetailResponseDto toProjectDetailResponse(PlaygroundProjectDetailResponse project) {
+		List<Link> links = project.links().stream()
+			.map(link -> new Link(LinkType.fromValue(link.linkTitle()), link.linkUrl()))
+			.toList();
+		Category category = new Category(project.category());
+		List<Member> members = project.members().stream()
+			.map(member -> new Member(
+				member.memberName(),
+				Role.fromValue(member.memberRole()),
+				member.memberDescription()
+			))
+			.toList();
+		String projectImage = project.images().isEmpty() ? null : project.images().get(0);
 
-        return ProjectsResponseDto.builder()
-                .id(project.id())
-                .name(project.name())
-                .generation(project.generation())
-                .category(category)
-                .serviceType(project.serviceType())
-                .summary(project.summary())
-                .detail(project.detail())
-                .logoImage(project.logoImage())
-                .thumbnailImage(project.thumbnailImage())
-                .isAvailable(project.isAvailable())
-                .isFounding(project.isFounding())
-                .links(links)
-                .build();
-    }
+		return ProjectDetailResponseDto.builder()
+			.id(project.id())
+			.name(project.name())
+			.generation(project.generation())
+			.category(category)
+			.projectImage(projectImage)
+			.serviceType(project.serviceType())
+			.summary(project.summary())
+			.detail(project.detail())
+			.logoImage(project.logoImage())
+			.thumbnailImage(project.thumbnailImage())
+			.isAvailable(project.isAvailable())
+			.isFounding(project.isFounding())
+			.links(links)
+			.startAt(project.startAt())
+			.endAt(project.endAt())
+			.uploadedAt(project.createdAt())
+			.updatedAt(project.updatedAt())
+			.members(members)
+			.build();
+	}
 
-
-    public ProjectDetailResponseDto toProjectDetailResponse(PlaygroundProjectDetailResponse project) {
-        List<Link> links = project.links().stream()
-                .map(link -> new Link(LinkType.fromValue(link.linkTitle()), link.linkUrl()))
-                .toList();
-        Category category = new Category(project.category());
-        List<Member> members = project.members().stream()
-                .map(member -> new Member(
-                        member.memberName(),
-                        Role.fromValue(member.memberRole()),
-                        member.memberDescription()
-                ))
-                .toList();
-        String projectImage = project.images().isEmpty() ? null : project.images().get(0);
-
-        return ProjectDetailResponseDto.builder()
-                .id(project.id())
-                .name(project.name())
-                .generation(project.generation())
-                .category(category)
-                .projectImage(projectImage)
-                .serviceType(project.serviceType())
-                .summary(project.summary())
-                .detail(project.detail())
-                .logoImage(project.logoImage())
-                .thumbnailImage(project.thumbnailImage())
-                .isAvailable(project.isAvailable())
-                .isFounding(project.isFounding())
-                .links(links)
-                .startAt(project.startAt())
-                .endAt(project.endAt())
-                .uploadedAt(project.createdAt())
-                .updatedAt(project.updatedAt())
-                .members(members)
-                .build();
-    }
-
-    public ReviewsResponseDto toReviewResponseDto(ReviewEntity entity) {
-        // subject List<String>을 단일 String으로 변환
-        String subjectStr;
-        List<String> subjects = entity.getSubject();
-        
-        if (subjects == null || subjects.isEmpty()) {
-            subjectStr = "";
-        } else if (subjects.size() == 1) {
-            // 배열 길이가 1이면 그 값만 사용
-            subjectStr = subjects.get(0);
-        } else {
-            // 배열 길이가 2 이상이면 " | "로 구분하여 하나의 문자열로 결합
-            subjectStr = String.join(" | ", subjects);
-        }
-        
-        return ReviewsResponseDto.builder()
-                .id(entity.getId())
-                .title(entity.getTitle())
-                .author(entity.getAuthor())
-                .authorProfileImageUrl(entity.getAuthorProfileImageUrl())
-                .generation(entity.getGeneration())
-                .description(entity.getDescription())
-                .part(entity.getPart())
-                .subject(subjectStr)
-                .thumbnailUrl(entity.getThumbnailUrl())
-                .platform(entity.getPlatform())
-                .url(entity.getUrl())
-                .build();
-    }
+	public ReviewsResponseDto toReviewResponseDto(ReviewEntity entity) {
+		
+		return ReviewsResponseDto.builder()
+			.id(entity.getId())
+			.title(entity.getTitle())
+			.category(entity.getCategory())
+			.author(entity.getAuthor())
+			.authorProfileImageUrl(entity.getAuthorProfileImageUrl())
+			.generation(entity.getGeneration())
+			.description(entity.getDescription())
+			.part(entity.getPart())
+			.subject(entity.getSubject())
+			.thumbnailUrl(entity.getThumbnailUrl())
+			.platform(entity.getPlatform())
+			.url(entity.getUrl())
+			.build();
+	}
 }
