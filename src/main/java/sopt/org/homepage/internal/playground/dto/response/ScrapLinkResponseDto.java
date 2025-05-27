de2d9bc9230a09feeca76e540b91b742a5973d58
찬기 <limchangi@naver.com>
2025-05-27 16:04:27 +0900

    feat: 솝티클 스크래핑을 위한 플그용 내부 스크랩 API 생성

---
+++ src/main/java/sopt/org/homepage/internal/playground/dto/response/ScrapLinkResponseDto.java
@@ -0,0 +1,26 @@
+package sopt.org.homepage.internal.playground.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "블로그 스크랩 정보 응답")
+public class ScrapLinkResponseDto {
+	@Schema(description = "블로그 스크랩 썸네일 이미지", requiredMode = Schema.RequiredMode.REQUIRED)
+	private String thumbnailUrl;
+
+	@Schema(description = "블로그 스크랩 제목", requiredMode = Schema.RequiredMode.REQUIRED)
+	private String title;
+
+	@Schema(description = "블로그 스크랩 설명", requiredMode = Schema.RequiredMode.REQUIRED)
+	private String description;
+
+	@Schema(description = "블로그 스크랩 URL", requiredMode = Schema.RequiredMode.REQUIRED)
+	private String url;
+}
