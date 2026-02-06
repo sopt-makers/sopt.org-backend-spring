bcff5eca442b4defbe495ee84088e40978f4d249
softmoca <acyc0909@gmail.com>
2026-02-06 16:24:07 +0900

    refactor : 사용하지 않는 메서드 제거

--- src/main/java/sopt/org/homepage/infrastructure/external/playground/PlaygroundService.java
+++ src/main/java/sopt/org/homepage/infrastructure/external/playground/PlaygroundService.java
@@ -1,10 +1,7 @@
 package sopt.org.homepage.infrastructure.external.playground;
 
 import java.util.List;
-
-import sopt.org.homepage.infrastructure.external.playground.dto.PlaygroundMemberListResponse;
 import sopt.org.homepage.infrastructure.external.playground.dto.PlaygroundProjectResponseDto;
-import sopt.org.homepage.infrastructure.external.playground.dto.PlaygroundUserResponse;
 import sopt.org.homepage.infrastructure.external.playground.dto.request.ScrapLinkRequestDto;
 import sopt.org.homepage.infrastructure.external.playground.dto.response.ScrapLinkResponseDto;
 import sopt.org.homepage.project.dto.request.GetProjectsRequestDto;
@@ -12,15 +9,11 @@ import sopt.org.homepage.project.dto.response.ProjectDetailResponseDto;
 import sopt.org.homepage.project.dto.response.ProjectsResponseDto;
 
 public interface PlaygroundService {
-    PlaygroundUserResponse getPlaygroundUserInfo(String authToken);
-
     List<ProjectsResponseDto> getAllProjects(GetProjectsRequestDto projectRequest);
 
     List<PlaygroundProjectResponseDto> getProjectsWithPagination();
 
     ProjectDetailResponseDto getProjectDetail(Long projectId);
-
-    PlaygroundMemberListResponse getAllMembers(Integer generation);
 
     ScrapLinkResponseDto scrapLink(ScrapLinkRequestDto scrapLinkRequestDto);
 }
